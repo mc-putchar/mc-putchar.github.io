a4cb2671bcbaddaba06edc829017c89b5b4afea9
Martin Cutura <mcutura@student.42berlin.de>
2023-07-02 17:56:38 +0200

    change line breaks

--- type.js
+++ type.js
@@ -6,10 +6,10 @@
 /*   By: mcutura <mcutura@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 17:38:59 by mcutura           #+#    #+#             */
-/*   Updated: 2023/07/02 17:53:54 by mcutura          ###   ########.fr       */
+/*   Updated: 2023/07/02 17:56:18 by mcutura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-const text = "GREETING> Hello! This is your website speaking\nREMINDER> You are a visitor\nALERT: You are being watched\nPOSITIVE AFFIRMATION> Good luck!";
+const text = "GREETING> Hello! This is your website speaking<br>REMINDER> You are a visitor<br>ALERT: You are being watched<br>POSITIVE AFFIRMATION> Good luck!";
 const typewriter = document.getElementById("typewriter");
 typewriter.innerHTML = text;
