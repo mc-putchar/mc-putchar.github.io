363ba33f99f462cd68f3c812fd7e291b5b542bca
Martin Cutura <mcutura@student.42berlin.de>
2023-07-02 17:54:16 +0200

    different approach to animation

--- type.js
+++ type.js
@@ -6,22 +6,10 @@
 /*   By: mcutura <mcutura@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 17:38:59 by mcutura           #+#    #+#             */
-/*   Updated: 2023/07/02 17:45:36 by mcutura          ###   ########.fr       */
+/*   Updated: 2023/07/02 17:53:54 by mcutura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 const text = "GREETING> Hello! This is your website speaking\nREMINDER> You are a visitor\nALERT: You are being watched\nPOSITIVE AFFIRMATION> Good luck!";
-const speed = 100; // Adjust the typing speed (in milliseconds)
-
-let index = 0;
 const typewriter = document.getElementById("typewriter");
-
-function putchar() {
-  if (index < text.length) {
-    typewriter.innerHTML += text.charAt(index);
-    index++;
-    setTimeout(putchar, speed);
-  }
-}
-
-putchar();
+typewriter.innerHTML = text;
