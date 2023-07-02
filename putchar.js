ea4e0cbaedcc7e14e1bf03e18ee582b599f0b6b7
Martin Cutura <mcutura@student.42berlin.de>
2023-07-02 18:20:32 +0200

    fix linebreaks #try3

--- putchar.js
+++ putchar.js
@@ -6,7 +6,7 @@
 /*   By: mcutura <mcutura@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 17:38:59 by mcutura           #+#    #+#             */
-/*   Updated: 2023/07/02 18:13:06 by mcutura          ###   ########.fr       */
+/*   Updated: 2023/07/02 18:20:15 by mcutura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,25 @@ const text = `~ $> GREETING> Hello! This is your website speaking
 const typewriter = document.getElementById("typewriter");
 
 function putchar() {
-  const lines = text.split("\n");
-  typewriter.textContent = ""; // Clear the existing content
+	const lines = text.split("\n");
+	typewriter.textContent = ""; // Clear the existing content
 
-  lines.forEach((line, index) => {
-    const lineElement = document.createElement("span");
-    lineElement.textContent = line;
-    lineElement.style.display = "block"; // Display each line as a block element
-
-    setTimeout(() => {
-      typewriter.appendChild(lineElement);
-    }, index * 1000); // Adjust the delay between lines if needed
-  });
+	lines.forEach((line, index) => {
+		const lineElement = document.createElement("span");
+		lineElement.textContent = line;
+		lineElement.style.display = "block"; // Display each line as a block element
+	
+		setTimeout(() => {
+			typewriter.appendChild(lineElement);
+	
+			if (index < lines.length - 1) {
+			const cursorElement = document.createElement("span");
+			cursorElement.textContent = "|";
+			cursorElement.style.opacity = "0"; // Hide the cursor initially
+			typewriter.appendChild(cursorElement);
+			}
+		}, index * 3000); // Adjust the delay between lines if needed
+	});
 }
 
 putchar();
