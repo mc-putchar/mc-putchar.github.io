706811d44a055df7b073d5aa868edfcf617bb671
Martin Cutura <mcutura@student.42berlin.de>
2024-06-12 01:41:35 +0200

    improve style

--- postchar.js
+++ postchar.js
@@ -6,10 +6,11 @@
 /*   By: mcutura <mcutura@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 23:20:12 by mcutura           #+#    #+#             */
-/*   Updated: 2024/06/08 22:39:56 by mcutura          ###   ########.fr       */
+/*   Updated: 2024/06/12 01:40:35 by mcutura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+const typewriter = document.getElementById("typewriter");
 const stdout = document.getElementById("stdout");
 const upFiles = document.querySelector("form");
 upFiles.addEventListener("submit", (event) => {
@@ -17,28 +18,65 @@ upFiles.addEventListener("submit", (event) => {
 	uploadFiles();
 });
 
-function postchar(chars) {
-	stdout.textContent = "";
+const text = `~ $CHALLENGE = We need ACTION!
+~ $INVITE = Choose your files
+~ $ACTION = Give 'em to me
+~ $??? = "trust me bro"
+~ $PROFIT = PROFIT!`;
+
+function putchar() {
+	const lines = text.split("\n");
+	typewriter.textContent = "";
+
+	lines.forEach((line, index) => {
+		const lineElement = document.createElement("span");
+		lineElement.textContent = line;
+		lineElement.style.display = "block";
 	
-	let index = 0;
-	const intervalId = setInterval(() => {
-		stdout.textContent += text[index];
-		index++;
-		
-		if (index >= text.length) {
-			clearInterval(intervalId);
-		}
-	}, 100);
+		setTimeout(() => {
+			typewriter.appendChild(lineElement);
+	
+			if (index < lines.length - 1) {
+			const cursorElement = document.createElement("span");
+			cursorElement.textContent = "|";
+			cursorElement.style.opacity = "0";
+			typewriter.appendChild(cursorElement);
+			}
+		}, index * 2500);
+	});
 }
 
-function uploadFiles() {
+putchar();
+
+async function uploadFiles() {
 	const url = new URL(form.action);
 	const fData = new FormData(form);
+	const size = Array.from(fData.keys()).length;
+	if (0 == size) {
+		postchar("No files selected :(");
+		return;
+	}
 
 	const fetchOptions = {
 		method: form.method,
 		body: fData,
 	};
-	fetch(url, fetchOptions);
-	postchar("Files uploaded succesfully");
+	postchar("Uploading " + size.toString() + " files...");
+
+	const response = await fetch(url, fetchOptions);
+	const status = "" + response.status + " - " + response.statusText;
+	const reply = await response.text();
+	postchar(status + '\n' + reply);
+}
+
+function postchar(chars) {
+	stdout.textContent = "";
+
+	let index = 0;
+	const intervalId = setInterval(() => {
+		stdout.textContent += chars[index++];
+		if (index >= chars.length) {
+			clearInterval(intervalId);
+		}
+	}, 100);
 }
